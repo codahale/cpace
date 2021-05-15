cc7a6646c1c61252e1fa59d3d9cfdac6d831721e
Coda Hale <coda.hale@gmail.com>
2021-05-15 16:21:20 -0600

    Add benchmark.

---
+++ benches/exchange_bench.rs
@@ -0,0 +1,31 @@
+use criterion::{black_box, Criterion, criterion_group, criterion_main};
+
+use cpace::{Initiator, Responder};
+
+fn bench_exchange(c: &mut Criterion) {
+    c.bench_function("exchange", |b| {
+        b.iter(|| {
+            let mut initiator = Initiator::new(
+                black_box(b"Alice"),
+                black_box(b"Bea"),
+                black_box(b"our shared secret"),
+            );
+            let (salt_a, a) = initiator.start();
+
+            let mut responder = Responder::new(
+                black_box(b"Bea"),
+                black_box(b"Alice"),
+                black_box(b"our shared secret"),
+            );
+            let (salt_b, b) = responder.start(salt_a, a);
+
+            let one = initiator.finish(salt_b, b);
+            let two = responder.finish();
+
+            (one, two)
+        })
+    });
+}
+
+criterion_group!(benches, bench_exchange);
+criterion_main!(benches);
