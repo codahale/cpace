2bae7e13be9a58af1a9a9cd6ce0dbc8faea4fdf6
Coda Hale <coda.hale@gmail.com>
2021-05-15 17:55:34 -0600

    Go all in on async.

---
+++ benches/cpace_bench.rs
@@ -0,0 +1,32 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use cpace::Exchanger;
+
+fn bench_exchanger(c: &mut Criterion) {
+    c.bench_function("exchanger", |b| {
+        b.iter(|| {
+            let alice = Exchanger::new(
+                black_box(b"Alice"),
+                black_box(b"Bea"),
+                black_box(b"our shared secret"),
+            );
+
+            let bea = Exchanger::new(
+                black_box(b"Bea"),
+                black_box(b"Alice"),
+                black_box(b"our shared secret"),
+            );
+
+            let y_alice = alice.send();
+            let y_bea = bea.send();
+
+            let alice = alice.receive(y_bea);
+            let bea = bea.receive(y_alice);
+
+            (alice, bea)
+        })
+    });
+}
+
+criterion_group!(benches, bench_exchanger);
+criterion_main!(benches);
